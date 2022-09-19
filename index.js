b434d23ce416ff01e2340f5542076f6c51a6d3f9
SmurfNiZed <zipacaldo@up.edu.ph>
2022-09-19 18:39:34 +0800

    Functional for all query types but no named ranges yet.

--- index.js
+++ index.js
@@ -11,10 +11,38 @@ function execute() {
     var mainSheet = ss.getSheetByName('Main Sheet');
     var resultSheet = ss.getSheetByName('Results');
     var url = mainSheet.getRange('B1').getValue();
+    var user = mainSheet.getRange('B2').getValue();
+    var lang = mainSheet.getRange('B3').getValue();
+    var prob = mainSheet.getRange('B4').getValue();
     if (!url) {
         ss.toast('No API endpoint specified', 'Error', 5);
     }
     else {
+        var clearOptions = {
+            contentsOnly: true
+        };
+        if (user || lang || prob) {
+            url += "?";
+        }
+        if (user) {
+            url += "user=" + user;
+            if (lang || prob) {
+                url += '&';
+            }
+        }
+        if (lang) {
+            url += "language=" + lang;
+            if (prob) {
+                url += '&';
+            }
+        }
+        if (prob) {
+            url += "problem=" + prob;
+        }
+        resultSheet.getRange('A2:F2').clear(clearOptions);
+        if (resultSheet.getMaxRows() > 2) {
+            resultSheet.deleteRows(3, resultSheet.getMaxRows() - 2);
+        }
         var response = UrlFetchApp.fetch(url);
         var str = response.getContentText();
         var obj = JSON.parse(str);
@@ -23,8 +51,10 @@ function execute() {
             var elem = _a[_i];
             matrix.push([elem.date, elem.problem, elem.user, elem.language, elem.result, elem.points]);
         }
-        var numRows = matrix.length;
-        var numCols = matrix[0].length;
-        resultSheet.getRange(2, 1, numRows, numCols).setValues(matrix);
+        if (matrix.length > 0) {
+            var numRows = matrix.length;
+            var numCols = matrix[0].length;
+            resultSheet.getRange(2, 1, numRows, numCols).setValues(matrix);
+        }
     }
 }
--- index.ts
+++ index.ts
@@ -14,12 +14,47 @@ function execute() {
   const resultSheet = ss.getSheetByName('Results');
 
   var url = mainSheet.getRange('B1').getValue();
+  var user = mainSheet.getRange('B2').getValue();
+  var lang = mainSheet.getRange('B3').getValue();
+  var prob = mainSheet.getRange('B4').getValue();
+
 
   if (!url) {
     ss.toast('No API endpoint specified', 'Error', 5);
   } 
   else
   {
+    const clearOptions = {
+      contentsOnly: true
+    };
+  
+    if(user || lang || prob) {
+      url += "?";
+    }
+  
+    if(user) {
+      url += "user=" + user;
+      if(lang || prob) {
+        url += '&';
+      }
+    }
+  
+    if(lang) {
+      url += "language=" + lang;
+      if(prob) {
+        url += '&';
+      }
+    }
+  
+    if(prob) {
+      url += "problem=" + prob;
+    }
+
+    resultSheet.getRange('A2:F2').clear(clearOptions);
+    if(resultSheet.getMaxRows() > 2) {
+      resultSheet.deleteRows(3, resultSheet.getMaxRows()-2)
+    }
+  
     const response = UrlFetchApp.fetch(url);
     const str = response.getContentText();
     const obj = JSON.parse(str);
@@ -29,12 +64,10 @@ function execute() {
     for(let elem of obj.data.objects) {
       matrix.push([elem.date, elem.problem, elem.user, elem.language, elem.result, elem.points]);
     }
-  
-    const numRows = matrix.length;
-    const numCols = matrix[0].length;
-    resultSheet.getRange(2, 1, numRows, numCols).setValues(matrix);
+    if (matrix.length > 0) {
+      const numRows = matrix.length;
+      const numCols = matrix[0].length;
+      resultSheet.getRange(2, 1, numRows, numCols).setValues(matrix);
+    }
   }
-
-  
-
 }
