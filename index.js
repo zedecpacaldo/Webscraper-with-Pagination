a80bfad4cb7a34ce6f92fa1c9cd36ea6f298717a
SmurfNiZed <zipacaldo@up.edu.ph>
2022-09-19 17:12:54 +0800

    Can Populate Results Sheet

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+// After it is working, check video at 1:40:41
+function onOpen() {
+    var ui = SpreadsheetApp.getUi();
+    // Or DocumentApp or FormApp.
+    ui.createMenu('Custom Menu')
+        .addItem('Execute', 'execute')
+        .addToUi();
+}
+function execute() {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var mainSheet = ss.getSheetByName('Main Sheet');
+    var resultSheet = ss.getSheetByName('Results');
+    var url = mainSheet.getRange('B1').getValue();
+    var response = UrlFetchApp.fetch(url);
+    var str = response.getContentText();
+    var obj = JSON.parse(str);
+    var matrix = [];
+    for (var _i = 0, _a = obj.data.objects; _i < _a.length; _i++) {
+        var elem = _a[_i];
+        matrix.push([elem.date, elem.problem, elem.user, elem.language, elem.result, elem.points]);
+    }
+    var numRows = matrix.length;
+    var numCols = matrix[0].length;
+    resultSheet.getRange(2, 1, numRows, numCols).setValues(matrix);
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+// After it is working, check video at 1:40:41
+
+function onOpen() {
+  const ui = SpreadsheetApp.getUi();
+  // Or DocumentApp or FormApp.
+  ui.createMenu('Custom Menu')
+      .addItem('Execute', 'execute')
+      .addToUi();
+}
+
+function execute() {
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  const mainSheet = ss.getSheetByName('Main Sheet');
+  const resultSheet = ss.getSheetByName('Results');
+
+  var url = mainSheet.getRange('B1').getValue();
+  const response = UrlFetchApp.fetch(url);
+  const str = response.getContentText();
+  const obj = JSON.parse(str);
+
+  const matrix: string[][] = [];
+
+  for(let elem of obj.data.objects) {
+    matrix.push([elem.date, elem.problem, elem.user, elem.language, elem.result, elem.points]);
+  }
+
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+  resultSheet.getRange(2, 1, numRows, numCols).setValues(matrix);
+
+}
